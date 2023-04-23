e4419cd6757bdfade5ddbc69d4c14a95c4c68406
anton <antonkovalev2019s@gmail.com>
2023-04-23 23:02:33 +0300

    2

---
+++ progi/homework/Program.cs
@@ -0,0 +1,74 @@
+﻿// Задача 1
+
+// Console.WriteLine("Input number A");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input number B");
+// int B = Convert.ToInt32(Console.ReadLine());
+
+// if (A>B)
+
+// {
+//    Console.WriteLine($"большее число - {A}, меньшее число - {B}");
+// }
+
+// else
+
+// {
+//     Console.WriteLine($"большее число - {B}, меньшее число - {A}");
+// }
+
+//  Задача 2
+
+// Console.WriteLine("Введите число A");
+// int A = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число B");
+// int B = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число С");
+// int C = Convert.ToInt32(Console.ReadLine());
+
+// if ((A>B) && (A>C)) 
+// {
+//     Console.WriteLine($"число {A} большее");
+// }
+// else if ((B>A) && (B>C))
+// {
+//     Console.WriteLine($"число {B} большее");
+// }
+// else if ((C>A) && (C>B))
+
+// {
+//     Console.WriteLine($"число {C} большее");
+// }
+
+// Задача 3
+
+// Console.WriteLine("Input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if (num % 2 == 0)
+// {
+//     Console.WriteLine($"{num} является четным");
+// }
+
+// else
+// {
+//     Console.WriteLine($"{num} не является четным");
+// }
+
+// Задача 4
+
+// Console.WriteLine("Input n");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// if (n<0) Console.WriteLine("Invalid input N");
+
+// int count = 2;
+
+// while(count <= n)
+// {
+    
+//     Console.Write(count+   " ");
+//     count++;
+// }
